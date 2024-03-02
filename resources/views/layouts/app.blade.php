c7c4b58b5e7f7420278915977ef9628e2eb54563
Mesakh Lolo <mesakhlolo12@gmail.com>
2024-03-02 21:31:17 +0700

    layouting menggunakan blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Bootstrap Javascript  -->
+    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <header class="p-3 text-bg-dark mb-3">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+                </ul>
+
+                <div class="text-end">
+                    <button type="button" class="btn btn-outline-light me-2">Login</button>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+
+        @yield('content')
+
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p class="col-md-4 mb-0 text-body-secondary">© 2024 Company, Inc</p>
+
+            <ul class="nav col-md-4 justify-content-end">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+            </ul>
+        </footer>
+
+    </div>
+</body>
+
+</html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,40 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-    <!-- Bootstrap CSS -->
-    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <!-- Bootstrap Javascript  -->
-    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
-</head>
+@section('title', 'Welcome Masbro')
 
-<body>
+@section('content')
+    <h1>The Best Blog Website Ever</h1>
+    <a href="{{ url('posts/create') }}" class="btn btn-success mb-3">+ Add Post</a>
 
-    <div class="container">
-        <h1>The Best Blog Website Ever</h1>
-        <a href="{{ url('posts/create') }}" class="btn btn-success mb-3">+ Add Post</a>
-
-        @foreach ($posts as $post)
-            <div class="card mb-3">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $post->title }}</h5>
-                    <p class="card-text">{{ $post->content }}</p>
-                    <p class="card-text"><small class="text-body-secondary">Last updated
-                            {{ date('d M Y H:i', strtotime($post->created_at)) }}</small></p>
-                    <a href="{{ url("posts/$post->id") }}" class="btn btn-primary">Selengkapnya</a>
-                    <a href="{{ url("posts/$post->id/edit") }}" class="btn btn-warning">Edit Cuy</a>
-                </div>
+    @foreach ($posts as $post)
+        <div class="card mb-3">
+            <div class="card-body">
+                <h5 class="card-title">{{ $post->title }}</h5>
+                <p class="card-text">{{ $post->content }}</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated
+                        {{ date('d M Y H:i', strtotime($post->created_at)) }}</small></p>
+                <a href="{{ url("posts/$post->id") }}" class="btn btn-primary">Selengkapnya</a>
+                <a href="{{ url("posts/$post->id/edit") }}" class="btn btn-warning">Edit Cuy</a>
             </div>
-        @endforeach
-
-    </div>
-
-</body>
-
-</html>
+        </div>
+    @endforeach
+@endsection
